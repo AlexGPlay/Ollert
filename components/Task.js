6fb9ff92a92e70c863769983d117dcac24e49ab0
alexgplay <alexgp1234@gmail.com>
2021-10-23 19:28:30 +0200

    Moved reorder logic to hook

--- components/Task.js
+++ components/Task.js
@@ -2,45 +2,12 @@ import { IconButton } from "@chakra-ui/button";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import { Box, Text } from "@chakra-ui/layout";
 import { useRef, useState } from "react";
-import { useDrag, useDrop } from "react-dnd";
+import { useTaskReorder } from "../hooks/useTaskReorder";
 
 const Task = ({ id, text, onEditClick, onDeleteClick, index, onMove }) => {
   const taskRef = useRef();
   const [hover, setHover] = useState();
-  const [{ handlerId }, drop] = useDrop({
-    accept: "task",
-    collect(monitor) {
-      return { handlerId: monitor.getHandlerId() };
-    },
-    hover(item, monitor) {
-      if (!taskRef.current) return;
-
-      const dragIndex = item.index;
-      const hoverIndex = index;
-
-      if (dragIndex === hoverIndex) return;
-
-      const hoverBoundingRect = taskRef.current?.getBoundingClientRect();
-      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-      const clientOffset = monitor.getClientOffset();
-      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) return;
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) return;
-
-      onMove(dragIndex, hoverIndex);
-      item.index = hoverIndex;
-    },
-  });
-
-  const [{ isDragging }, drag] = useDrag({
-    type: "task",
-    item: () => ({ index, id }),
-    collect: (monitor) => ({ isDragging: monitor.isDragging() }),
-  });
-
-  drag(drop(taskRef));
+  const { isDragging } = useTaskReorder(taskRef, id, index, onMove);
 
   return (
     <Box
