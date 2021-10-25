2d5a92d8272abb8da0681a61f56d821b81a77458
alexgplay <alexgp1234@gmail.com>
2021-10-25 22:59:34 +0200

    Added useIsMobile and TouchBackend for mobile devices DnD

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
+import { useIsMobile } from "../hooks/useIsMobile";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const isMobile = useIsMobile();
+
   return (
     <ChakraProvider>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
         <Component {...pageProps} />
       </DndProvider>
     </ChakraProvider>
